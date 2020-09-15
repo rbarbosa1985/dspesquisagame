426f538f8be3fcc1f8afdfe50a3fd35684534f09
Roberto Barbosa <betoanselmo@gmail.com>
2020-09-15 20:46:34 -0300

    Project Created

---
+++ backend/src/main/java/com/devsuperior/dspesquisagame/DspesquisagameApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dspesquisagame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DspesquisagameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DspesquisagameApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dspesquisagame/DspesquisagameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dspesquisagame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisagameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
