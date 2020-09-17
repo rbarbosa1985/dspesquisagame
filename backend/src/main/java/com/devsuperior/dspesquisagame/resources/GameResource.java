c95276a931df546fd288529ec065386d311074a3
Roberto Barbosa <betoanselmo@gmail.com>
2020-09-17 13:54:35 -0300

    DTO e Service

--- backend/src/main/java/com/devsuperior/dspesquisagame/resources/GameResource.java
+++ backend/src/main/java/com/devsuperior/dspesquisagame/resources/GameResource.java
@@ -8,17 +8,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dspesquisagame.entities.Game;
-import com.devsuperior.dspesquisagame.repositories.GameRepository;
+import com.devsuperior.dspesquisagame.dto.GameDTO;
+import com.devsuperior.dspesquisagame.services.GameService;
 
 @RestController
 @RequestMapping(value = "/games")
 public class GameResource {
 	
+	@Autowired
+	private GameService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Game>> findAll(){
-		List<Game> list = gameRepository.findAll();
+	public ResponseEntity<List<GameDTO>> findAll(){
+		List<GameDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
